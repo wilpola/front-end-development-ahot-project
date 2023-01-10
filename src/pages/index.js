7e93b5c854a8d2de7b67022742d6824de130f1db
wilpola <wilpolaproduction@gmail.com>
2023-01-10 12:01:59 +0200

    Adds pages to export file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,11 @@
 // Single point of imports and exports for pages
+
+// Import pages
+import Home from "./Home";
+import About from "./About";
+
+// Export all the pages
+export {
+    Home,
+    About
+}
