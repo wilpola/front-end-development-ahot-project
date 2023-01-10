df0b919db18fc175203a0ffd8d9964a8332b7ee3
wilpola <wilpolaproduction@gmail.com>
2023-01-10 12:01:24 +0200

    Adds Home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+// This file contains the home page
+
+const Home = () => {
+
+    return(
+        <div id={'Home'}>
+            <h1> Home </h1>
+        </div>
+    )
+}
+
+// exports the page
+export default Home;
