e5f0daff46ced6e74d12689b7e8b2970f521d02e
wilpola <wilpolaproduction@gmail.com>
2023-01-10 12:01:07 +0200

    Adds About page

---
+++ src/pages/About/index.js
@@ -0,0 +1,13 @@
+// This file contains the About page
+
+const About = () => {
+
+    return (
+        <div id={"About"}>
+            <h1>About</h1>
+        </div>
+    )
+}
+
+// export about page
+export default About;
